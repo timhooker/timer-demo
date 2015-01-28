222932894abc7874317bb297f12f06abafa426b1
Tim Hooker <timhooker@me.com>
2015-01-28 10:27:18 -0500

    made some sweet interval changes

--- js/timer.js
+++ js/timer.js
@@ -1,10 +1,21 @@
 app.Timer = function (spec) {
-  var interval = spec.interval;
+  // INterval defaults to 1 second
+  var interval = spec.interval || 1000;
+
+  // Callback is the logic that we want to run
+  // every time the timer fires
   var callback = spec.callback;
+
 
   var self = {
     start: function(){
-
+      callback();
+      function intervalElapsed(){
+        callback();
+        setTimout(callback, interval);
+        timeoutID = setTimeout(intervalElapsed, interval);
+      }
+      setTimeout(callback, interval);
     }
 
     stop: function(){
